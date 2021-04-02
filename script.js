8ab0c7c428d2df56eaaa22d509056ed32399bf70
Thomas Lincoln <thmas5952@gmail.com>
2021-04-02 17:01:33 -0300

    Adição da variáveis da carta

---
+++ script.js
@@ -0,0 +1,28 @@
+var cartas = [
+    {
+        nome: "Dragonpit Magician",
+        imagem: "https://static.wikia.nocookie.net/yugioh/images/6/63/DragonpitMagician-PEVO-EN-SR-1E.png/revision/latest/scale-to-width-down/300?cb=20170621141225",
+        atributos: {
+            defesa: 2700,
+            ataque: 900
+        }
+    },
+    {
+        nome: "Odd-Eyes Arc Pendulum Dragon",
+        imagem: "https://static.wikia.nocookie.net/yugioh/images/8/8b/OddEyesArcPendulumDragon-LEDD-EN-UR-1E.png/revision/latest/scale-to-width-down/300?cb=20171005215856",
+        atributos: {
+            defesa: 2000,
+            ataque: 2700
+        }
+    },
+    {
+        nome: "Lula",
+        imagem: "http://orig11.deviantart.net/cab5/f/2009/181/0/4/lula_card_by_alexxtm.jpg",
+        atributos: {
+            defesa: 2400,
+            ataque: 3400
+        }
+    }
+]
+
+

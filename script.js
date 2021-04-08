290951079dedaa1e6f4528da9b385e1f141c5012
Thomas Lincoln <thmas5952@gmail.com>
2021-04-08 15:41:54 -0300

    Adição da API yugioh pro deck

--- script.js
+++ script.js
@@ -1,78 +1,55 @@
-var cartas = [
-    {
-        nome: "Dragonpit Magician",
-        imagem: "https://www.duelshop.com.br/8914/dragonpit-magician-pevo-en014-super-rare.jpg",
-        atributos: {
-            defesa: 2700,
-            ataque: 900
-        }
-    },
-    {
-        nome: "Odd-Eyes Arc Pendulum Dragon",
-        imagem: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/627fe721-846f-4f75-ac61-111ca00b27dd/dbopqe5-7a01dd11-507a-4691-9ac7-80fb4e703318.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvNjI3ZmU3MjEtODQ2Zi00Zjc1LWFjNjEtMTExY2EwMGIyN2RkXC9kYm9wcWU1LTdhMDFkZDExLTUwN2EtNDY5MS05YWM3LTgwZmI0ZTcwMzMxOC5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.9NwsE1ukKqqVlOF-qI1zh45oM3q27Juooz3SmIhZBGI",
-        atributos: {
-            defesa: 2000,
-            ataque: 2700
-        }
-    },
-    {
-        nome: "Lula",
-        imagem: "http://orig11.deviantart.net/cab5/f/2009/181/0/4/lula_card_by_alexxtm.jpg",
-        atributos: {
-            defesa: 2400,
-            ataque: 3400
-        }
-    }
-]
+"use strict"
+
+var atributoSelecionado
+var atributoNaoSelecionado
 var imagemPlayer = document.getElementById('cartaPlayer')
-var imagemMaquina = document.getElementById('cartaMaquina')
-var numeroCartaMaquina = 0
-var cartaJogadorSelecionado = 0
 var imagemMaquina = document.getElementById('cartaMaquina')
 var pontosMaquina = 0
 var pontosPlayer = 0
+var numRandom
 var radioAtributo = document.getElementsByClassName("escolha")
+let listaDeCards = {};
+let cartaMaquina 
+let cartaJogador
 
+getAllCards()
+
+async function getAllCards() {
+    await fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php?atk=gt&def=gt")
+    //converte respose para object
+        .then((response) => response.json())
+        //coloca os valores de id dos elementos em yugidata
+        .then((yugioh) => {
+            listaDeCards = yugioh.data.map((el) => {
+                return {
+                    id: el.id,
+                    imagem: el.card_images[0].image_url,
+                    ataque: el.atk,
+                    defesa: el.def
+                }
+            });
+        })
+        .catch(() => {
+            console.log("Deu ruim!");
+        });
+}
 
 function sortearCarta() {
-    numeroCartaMaquina = 0
-    numeroCartaJogador = 0
     imagemMaquina.src = "https://upload.wikimedia.org/wikipedia/en/thumb/2/2b/Yugioh_Card_Back.jpg/150px-Yugioh_Card_Back.jpg";
-
-    numeroCartaMaquina = parseInt(Math.random() * 3)
-    numeroCartaJogador = parseInt(Math.random() * 3)
-    cartaMaquina = cartas[numeroCartaMaquina]
-
-    if (numeroCartaJogador == numeroCartaMaquina) {
-        numeroCartaJogador = parseInt(Math.random() * 3)
-    } else {
-        cartaJogador = cartas[numeroCartaJogador]
-    }
+    
+    //pega um index random da lista de cards
+    numRandom = parseInt(Math.random() * listaDeCards.length)
+    cartaJogador = listaDeCards[numRandom]
+    listaDeCards.splice(numRandom, 1)
+    numRandom = parseInt(Math.random() * listaDeCards.length)
+    cartaMaquina = listaDeCards[numRandom]
+    listaDeCards.splice(numRandom, 1)
 
     document.getElementById('btnSortear').disabled = true
     document.getElementById('btnDecidir').disabled = false
     document.getElementById('btnDecidir').style.backgroundColor = "rgb(206, 168, 85)"
     document.getElementById('btnSortear').style.backgroundColor = "gray"
-}
-
-function exibeCarta() {
-    sortearCarta()
-    if (numeroCartaMaquina == numeroCartaJogador) {
-        sortearCarta()
-    } else {
-        for (var i = 0; i < cartas.length; i++) {
-            if (numeroCartaJogador == i) {
-                imagemPlayer.src = cartas[i].imagem
-            }
-        }
-    }
-}
-function exibeCartaMaquina() {
-    for (var i = 0; i < cartas.length; i++) {
-        if (numeroCartaMaquina == i) {
-            imagemMaquina.src = cartas[i].imagem
-        }
-    }
+    imagemPlayer.src = cartaJogador.imagem
 }
 
 
@@ -92,24 +69,24 @@ function reiniciar() {
 function pegaAtributo() {
     for (var i = 0; i < radioAtributo.length; i++) {
         if (radioAtributo[i].checked) {
-            return radioAtributo[i].value
+            atributoSelecionado = radioAtributo[i].value
+        }
+        if(!radioAtributo[i].checked){
+            atributoNaoSelecionado = radioAtributo[i].value
         }
     }
 }
 
 function btnDecidir() {
-    var atributoSelecionado = pegaAtributo()
-
-    if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
-        console.log("ganhou")
+    pegaAtributo()
+    if (cartaJogador[atributoSelecionado] > cartaMaquina[atributoNaoSelecionado]) {
         pontosPlayer++
         document.getElementById('pPlayer').innerHTML = pontosPlayer
-        exibeCartaMaquina()
-    } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
-        console.log("perdeu")
+        imagemMaquina.src = cartaMaquina.imagem
+    } else if (cartaJogador[atributoSelecionado] < cartaMaquina[atributoNaoSelecionado]) {
         pontosMaquina++
         document.getElementById('pMaquina').innerHTML = pontosMaquina
-        exibeCartaMaquina()
+        imagemMaquina.src = cartaMaquina.imagem
     }
     document.getElementById('btnSortear').disabled = false
     document.getElementById('btnDecidir').disabled = true
